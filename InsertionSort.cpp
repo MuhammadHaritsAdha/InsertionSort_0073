ad038474922680aad5a7e5405cb91b97cb351a65
MuhammadHaritsAdha <mharitsadha@gmail.com>
2025-03-24 15:42:14 +0700

    Commit 2

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -11,3 +11,16 @@ void input()
 cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan Jumlah element Array
 cin >> n; // Memanggil variable inputan n
 
+if (n <= 20)
+{// membuat konidsi n tidak lebih besar dari 20}
+break;
+}
+else
+{
+cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // menampilkan Pesan jika data lebih dari 20
+}
+}
+cout << endl; // Membuat jarak per baris program
+cout << "======================" << endl; // Membuat tampilan susunan data element array
+cout << "Masukkan Element Array" << endl;
+cout << "======================" << endl;
