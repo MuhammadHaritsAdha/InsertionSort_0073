9c67838a8920d09111a443e165449c72bc373458
MuhammadHaritsAdha <mharitsadha@gmail.com>
2025-03-24 15:40:08 +0700

    Commit 1

---
+++ InsertionSort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; /// Membuat Array dengan panjang data 20
+int n; // Membuat Variable Inputan n
+
+void input()
+{ // Procedure Input
+    while (true)
+{
+cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan Jumlah element Array
+cin >> n; // Memanggil variable inputan n
+
